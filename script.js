33b5046d8ec3ec049d6713ae4137f5c0abef1049
xAkihiro666 <86543167+xAkihiro666@users.noreply.github.com>
2024-09-18 09:39:00 +0800

    Updated Login

--- script.js
+++ script.js
@@ -13,29 +13,52 @@ const nextBtn = document.getElementById("next");
 const loopBtn = document.getElementById("loop");
 const tpass = document.getElementById("password");
 const temail = document.getElementById("email");
-const login = document.getElementById("logMe");
-const register = document.getElementById("regMe");
 const logForm = document.querySelector('.login');
 const regForm = document.querySelector('.register');
 const holder = document.querySelector('.log');
-const tHold = document.querySelector('.testing');
 const btns = document.querySelector('.Btns');
 
-btns.addEventListener('click', () => {
-  if(tpass.value != '' && temail.value != ''){
+// Sunod nani uy
+const usernames = ["akihiroakio47@gmail.com", "judeko@gmail.com"];
+const passwords = ["12345", "9851"];
+
+btns.addEventListener('click', (e) => {
+  const email = temail.value;
+  const password = tpass.value;
+
+  if (!isValidEmail(email)) {
+    swal("Error", "Invalid email format.", "error");
+    e.preventDefault();
+  } else if (!Checker(email, password)) {
+    swal("Error", "Incorrect Email or Password.", "error");
+  } else if (email !== '' && password !== '') {
     logForm.style.display = 'none';
     holder.style.display = 'none';
-    loginSucces();
-  }else if(temail.value != ''){
-
+    loginSuccess();
+  } else {
+    swal("Error", "Email and Password cannot be empty.", "error");
   }
-})
+});
 
-function loginSucces(){
-  if(tpass.value != ''){
-      swal("Success", "You have successfully logged in.", "success");
+function Checker(email, password) {
+  const index = usernames.indexOf(email);
+
+  if (index !== -1 && passwords[index] === password) {
+    return true;
+  } else {
+    return false;
   }
 }
+
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+
+function loginSuccess() {
+  swal("Success", "You have successfully logged in.", "success");
+}
+
 
 const playerImg = document.querySelector('.player_img img');
 const songs = [
